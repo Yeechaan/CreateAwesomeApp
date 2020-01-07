87c7c64b31d789f019d5931e7e7e96964fefc154
YechanLee <yechanharry@gmail.com>
2020-01-07 23:37:19 +0900

    Get Picture Permission

---
+++ PicturePermission.java
@@ -0,0 +1,45 @@
+﻿package com.lee.oneweekonebook;
+
+import android.Manifest;
+import android.content.Intent;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    AdapterRead adapter;
+    SQLiteDatabase database;
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //사진 권한
+        requirePermission();
+
+//Manifest에 권한 추가
+//<uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+//<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+//<uses-permission android:name="android.permission.CAMERA" />
+
+    }
+
+    //권한 확인 모듈
+    void requirePermission() {
+        String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        ArrayList<String> listPermissionsNeeded = new ArrayList<>();
+
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
+                //권한이 허가가 안됬을 경우 요청할 권한을 모집하는 부분
+                listPermissionsNeeded.add(permission);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            //권한 요청 하는 부분
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 1);
+        }
+    }
+}
