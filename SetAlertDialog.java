b3af9df88964658ba3e218abbdc17e6202e09ddf
YechanLee <yechanharry@gmail.com>
2020-01-16 12:28:14 +0900

    Set Alert Dialog on Android

---
+++ SetAlertDialog.java
@@ -0,0 +1,56 @@
+﻿package com.lee.oneweekonebook;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    AdapterRead adapter;
+    SQLiteDatabase database;
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //알림창 띄우기
+        Button btn_read_add = (Button) findViewById(R.id.btn_read_add);
+        btn_read_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("종료 하시겠습니까?");
+       	    builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                   @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //DB에 저장 후 "독서-done" 으로 이동
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
+}
