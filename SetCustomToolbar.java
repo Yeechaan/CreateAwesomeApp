4a42d3d17e299fc658e10f958a3a997779ffb767
YechanLee <yechanharry@gmail.com>
2020-01-20 16:53:49 +0900

    Set Custom Toolbar on Android

---
+++ SetCustomToolbar.java
@@ -0,0 +1,47 @@
+﻿package com.lee.oneweekonebook;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class DoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done);
+
+        //툴바 관련
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+
+        setSupportActionBar(toolbar);
+        mTitle.setText("다 읽은 책 목록");
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        //뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    //Toolbar 백버튼 이벤트 처리
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                //main 으로 이동
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
